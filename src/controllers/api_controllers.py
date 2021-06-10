326243433f0f6528b404c2af12c0db111c31c382
Kim Greenbush <kim_greenbush@icloud.com>
2021-06-10 07:45:27 -0700

    Initial

---
+++ src/controllers/api_controllers.py
@@ -0,0 +1,72 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+# POST /clients => Creates a new client return a 201 status code
+@app.route("/clients", methods=["POST"])
+def new_client():
+    pass
+
+# GET /clients => gets all clients return 200
+@app.route("/")
+def get_clients():
+    pass
+
+# GET /clients/10 => get client with id of 10 return 404 if no such client exist
+@app.route("/clients/<id>")
+def get_client(id):
+    pass
+
+# PUT /clients/12 => updates client with id of 12 return 404 if no such client exist
+@app.route("/clients/<id>", methods=["PUT"])
+def update_client(id):
+    pass
+
+# DELETE /clients/15 => deletes client with the id of 15 return 404 if no such client exist return 205 if success
+@app.route("/clients/<id>", methods=["DELETE"])
+def delete_client(id):
+    pass
+
+# POST /clients/5/accounts =>creates a new account for client with the id of 5 return a 201 status code
+@app.route("/clients/<id>/accounts", methods=["POST"])
+def add_client_account(id):
+    pass
+
+# GET /clients/7/accounts => get all accounts for client 7 return 404 if no client exists
+@app.route("/clients/<id>/accounts")
+def get_client_accounts(id):
+    pass
+
+# GET /clients/7/accounts?amountLessThan=2000&amountGreaterThan400 => get all accounts for client 7 between 400 and 2000 return 404 if no client exists
+@app.route("/clients/<id>/accounts")
+def get_client_accounts_between_400_2000(id):
+    pass
+
+# GET /clients/9/accounts/4 => get account 4 for client 9 return 404 if no account or client exists
+@app.route("/clients/<client_id>/accounts/<account_id>")
+def get_client_account(client_id, account_id):
+    pass
+
+# PUT /clients/10/accounts/3 => update account with the id 3 for client 10 return 404 if no account or client exists
+@app.route("/clients/<client_id>/accounts/<account_id>", methods=["PUT"])
+def update_client_account(client_id, account_id):
+    pass
+
+# DELETE /clients/15/accounts/6 => delete account 6 for client 15 return 404 if no account or client exists
+@app.route("/clients/<client_id>/accounts/<account_id>", methods=["DELETE"])
+def delete_client_account(client_id, account_id):
+    pass
+
+# PATCH /clients/17/accounts/12 => Withdraw/deposit given amount (Body: {"deposit":500} or {"withdraw":250} return 404 if no account or client exists return 422 if insufficient funds
+@app.route("/clients/<client_id>/accounts/<account_id", methods=["PATCH"])
+def update_client_account_balance(client_id, account_id):
+    pass
+
+# PATCH /clients/12/accounts/7/transfer/8 => transfer funds from account 7 to account 8 (Body: {"amount":500}) return 404 if no client or either account exists return 422 if insufficient funds
+@app.route("/clients/<client_id>/accounts/<account1_id>/transfer/<account2_id>")
+def  transfer_between_accounts(client_id, account1_id, account2_id):
+    pass
+
+if __name__ == '__main__':
+    app.run(debug=True)
