1f7fd84ae311faddfce915cfe5b634fec06cbea3
Kim Greenbush <kim_greenbush@icloud.com>
2021-06-15 09:50:28 -0700

    add Account properties, methods

--- src/models/account.py
+++ src/models/account.py
@@ -1,2 +1,31 @@
+from src.db import metadata
+from sqlalchemy import Table, Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+
 class Account:
-    pass
+    __tablename__ = "account"
+    acount_table = Table(
+    "account",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("client_id", ForeignKey("client.id"), nullable=False),
+    Column("balance", Integer),
+    Column("type", String(15))
+    )
+    owner = relationship("Client", back_populates="accounts")
+
+    def __init__(self, id, client_id, init_deposit, type):
+        self._account_id = id
+        self._client_id = client_id
+        self._balance = init_deposit
+        self._type = type
+
+    def deposit(self, amount):
+        self._balance += amount
+
+    def withdraw(self, amount):
+        self._balance -= amount
+
+    def transfer(self, other_acct, amount):
+        self._balance -= amount
+        other_acct._balance += amount
