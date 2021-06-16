eab08b859429a9be9bed5c2c3eb4f1e8b1f8881c
Kim Greenbush <kim_greenbush@icloud.com>
2021-06-16 01:26:33 -0700

    add db connection

--- src/db.py
+++ src/db.py
@@ -1,5 +1,9 @@
-from sqlalchemy import create_engine, MetaData
+from sqlalchemy import create_engine, MetaData, text
 
-engine = create_engine("", echo=True, future=True)
+engine = create_engine("postgresql+psycopg2://bankingapi:Soubikun.20@bankingapi.cpiiv9dpoug5.us-east-2.rds.amazonaws.com:5432/pgdb", echo=True, future=True)
+
+with engine.connect() as conn:
+    result = conn.execute(text("select 'hello world"))
+    print()
 
 metadata = MetaData
