33d8648eab256afe2ba1bcfe674d878fe1894c18
Kim Greenbush <kim_greenbush@icloud.com>
2021-06-18 15:32:25 -0700

    remove sqlalchemy

--- src/db.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine, MetaData, text
-
-engine = create_engine("postgresql+psycopg2://bankingapi:Soubikun.20@bankingapi.cpiiv9dpoug5.us-east-2.rds.amazonaws.com:5432/pgdb", echo=True, future=True)
-
-with engine.connect() as conn:
-    result = conn.execute(text("select 'hello world"))
-    print()
-
-metadata = MetaData
